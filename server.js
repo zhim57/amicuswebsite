3420a4e25f8c570406b713303f749890e9b48058
Jivko Atanassov <zhim57@yahoo.com>
2025-07-06 10:42:55 -0400

    Add Jest testing framework and basic server tests

--- server.js
+++ server.js
@@ -54,6 +54,11 @@ app.get('/files', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
