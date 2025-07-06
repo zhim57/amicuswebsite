98efe70c0022f7b9b8808bfca2c945259bb2bb63
Jivko <zhim57@yahoo.com>
2025-07-06 10:07:05 -0400

    port

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/files', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3015;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
